1e6807347f92d92598688899e94e03d4ceef02e5
Darshan goswami <goswamidarshan12345@gmail.com>
2026-02-10 10:35:16 +0530

    add frontend and tailwind and full frotend project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <button className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Click me</button>
+    </>
+  )
+}
+
+export default App
